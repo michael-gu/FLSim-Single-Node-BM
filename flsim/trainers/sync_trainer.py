cc9699c5da0c53065543a03e61898a001f00606c
Michael Gu <michgu@uw.edu>
2024-01-30 11:20:25 -0800

    test

--- flsim/trainers/sync_trainer.py
+++ flsim/trainers/sync_trainer.py
@@ -190,7 +190,7 @@ class SyncTrainer(FLTrainer):
         """
         # Set up synchronization utilities for distributed training
         database_helper.delete_table('model_databases/flsim_single_node_models.db', 'cifar10_models')
-        mysql_database_helper.delete_table('localhost', 'michgu', 'Dolphin#1', 'cifar10_benchmarks', 'cifar10_models')
+        # mysql_database_helper.delete_table('localhost', 'michgu', 'Dolphin#1', 'cifar10_benchmarks', 'cifar10_models')
         FLDistributedUtils.setup_distributed_training(
             distributed_world_size, use_cuda=self.cuda_enabled
         )  # TODO do not call distributed utils here, this is upstream responsibility
