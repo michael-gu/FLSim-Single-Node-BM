4e7c506574f6de9ed8da1db62e6769da46b167ef
Michael Gu <michgu@uw.edu>
2024-01-30 13:43:28 -0800

    implemented csv storage for mysql migration

---
+++ examples/get_stats_mysql.py
@@ -0,0 +1,45 @@
+import mysql.connector
+from flsim.mysql_database_helper import get_benchmark_stats
+
+# Connect to the database
+conn = mysql.connector.connect(
+    host='your_host',
+    user='your_username',
+    password='your_password',
+    database='your_database'
+)
+
+# Create a cursor object
+cursor = conn.cursor()
+
+# Execute the query to check if the table exists
+cursor.execute("SHOW TABLES LIKE 'benchmarks_yes_tracking'")
+
+# Fetch the result
+result = cursor.fetchone()
+
+# Check if the table exists
+if result:
+    print("benchmarks_yes_tracking exists")
+    csv_path = "../benchmark_stats/single-node-with-feature.csv"
+    get_benchmark_stats('localhost', 'michgu', 'Dolphin#1', 'your_database', 'benchmarks_yes_tracking', csv_path)
+else:
+    print("benchmarks_yes_tracking does not exist")
+
+
+# Execute the query to check if the table exists
+cursor.execute("SHOW TABLES LIKE 'benchmarks_no_tracking'")
+
+# Fetch the result
+result = cursor.fetchone()
+
+# Check if the table exists
+if result:
+    print("benchmarks_no_tracking exists")
+    csv_path = "../benchmark_stats/single-node-without-feature.csv"
+    get_benchmark_stats('localhost', 'michgu', 'Dolphin#1', 'your_database', 'benchmarks_no_tracking', csv_path)
+else:
+    print("benchmarks_no_tracking does not exist")
+
+# Close the connection
+conn.close()
