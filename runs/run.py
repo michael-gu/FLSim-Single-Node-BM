19729ba33744df20dea404725c9636d3446a574f
Michael Gu <michgu@cs.washington.edu>
2024-02-01 12:35:23 -0800

    fixes

--- runs/run.py
+++ runs/run.py
@@ -4,14 +4,17 @@ import argparse
 
 # Parse command-line arguments
 parser = argparse.ArgumentParser()
+parser.add_argument("dataset", help="dataset")
 parser.add_argument("mode", help="Mode to run the script in. 'true' or 'test'.")
 parser.add_argument("range_value", type=int, nargs='?', default=1, help="The range value to use in the loop.")
 args = parser.parse_args()
 
 # Set the command based on the mode argument
+dataset = str(args.dataset)
 if args.mode == "true":
-    command = "python3 cifar10_example.py --config-file configs/cifar10_config_with_feature.json"
+    command = "python3 " + dataset + "_example.py --config-file configs/" + dataset + "_config_with_feature.json"
     try:
+        os.chdir('../examples')
         for _ in range(args.range_value):
             if os.system(command) != 0:
                 print("Command failed, exiting.")
@@ -20,8 +23,9 @@ if args.mode == "true":
         print("Interrupted by user, exiting.")
         sys.exit(1)
 elif args.mode == "false":
-    command = "python3 cifar10_example.py --config-file configs/cifar10_config_without_feature.json"
+    command = "python3 " + dataset + "_example.py --config-file configs/" + dataset + "_config_without_feature.json"
     try:
+        os.chdir('../examples')
         for _ in range(args.range_value):
             if os.system(command) != 0:
                 print("Command failed, exiting.")
