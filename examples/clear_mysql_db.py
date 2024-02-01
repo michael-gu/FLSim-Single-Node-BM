d861df19140fb54cb65b6f78628884b0fa173fe8
Michael Gu <michgu@cs.washington.edu>
2024-02-01 13:29:50 -0800

    test

--- examples/clear_mysql_db.py
+++ examples/clear_mysql_db.py
@@ -1,3 +1,3 @@
 import flsim.mysql_database_helper as database_helper
 
-database_helper.clear_model_db('localhost', 'michgu', 'Dolphin#1', 'cifar10_benchmarks')
+database_helper.clear_model_db('localhost', 'michgu', 'Dolphin#1', 'benchmarks')
