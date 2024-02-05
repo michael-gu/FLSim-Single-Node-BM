fa3eeca44ec3e151aa446b84b4fd122c9cf16281
Michael Gu <michgu@cs.washington.edu>
2024-02-05 00:01:30 -0800

    sent140 setup init commit

---
+++ runs/sent140_setup.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+try:
+    os.chdir('../examples')
+    if os.system("chmod +x get_data.sh"):
+        print("get_data.sh permissions change failed, exiting.")
+        sys.exit(1)
+    if os.system("sudo ./get_data.sh"):
+        print("get_sent140_data failed, exiting")
+        sys.exit(1)
+except KeyboardInterrupt:
+    print("Interrupted by user, exiting.")
+    sys.exit(1)
+
