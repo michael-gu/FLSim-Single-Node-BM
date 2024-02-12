20cf9ab98a764f434c87475769831e1a488a9f8c
Michael Gu <michgu@cs.washington.edu>
2024-02-11 17:14:43 -0800

    test

--- examples/get_data.sh
+++ examples/get_data.sh
@@ -7,4 +7,5 @@
 
 git clone https://github.com/TalwalkarLab/leaf.git
 cd leaf/data/sent140 || exit
+gdown --id 1JvbbjHTrMWlp_FX_qHkLsOCvCQfNjNfD -O trainingandtestdata.zip
 ./preprocess.sh --sf 0.01 -s niid -t 'user' --tf 0.90 -k 1 --spltseed 1
