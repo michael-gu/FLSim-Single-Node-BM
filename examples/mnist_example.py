94d833c48ac23ca555f696af5030be9a25a1e792
Michael Gu <michgu@cs.washington.edu>
2024-02-12 12:54:52 -0800

    test

--- examples/mnist_example.py
+++ examples/mnist_example.py
@@ -42,6 +42,7 @@ from hydra.utils import instantiate
 from omegaconf import DictConfig, OmegaConf
 from torchvision import transforms
 from torchvision.datasets import MNIST
+import torchvision.models as models
 
 IMAGE_SIZE = 32
 
@@ -83,10 +84,11 @@ def build_data_provider(local_batch_size, examples_per_user, drop_last: bool = F
 def main(trainer_config, data_config, use_cuda_if_available: bool = True,) -> None:
     cuda_enabled = torch.cuda.is_available() and use_cuda_if_available
     device = torch.device(f"cuda:{0j}" if cuda_enabled else "cpu")
-    model = Resnet18(num_classes=10)
-    model.conv1 = torch.nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+    #model = Resnet18(num_classes=10)
     # model = SimpleConvNet(in_channels=3, num_classes=10)
-
+    
+    model = models.resnet18(pretrained=False)
+    model.conv1 = torch.nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
 
     # Create the parser
     parser = argparse.ArgumentParser()
