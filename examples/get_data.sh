16d79c26d0b8d2cc7c61d8221e9d6f7235e9befa
Michael Gu <michgu@cs.washington.edu>
2024-02-11 18:20:07 -0800

    test

--- examples/get_data.sh
+++ examples/get_data.sh
@@ -5,8 +5,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-git clone https://github.com/TalwalkarLab/leaf.git
+git clone https://github.com/michael-gu/leaf.git
 cd leaf/data/sent140 || exit
-gdown --id 1JvbbjHTrMWlp_FX_qHkLsOCvCQfNjNfD -O trainingandtestdata.zip
-unzip trainingandtestdata
+
 ./preprocess.sh --sf 0.01 -s niid -t 'user' --tf 0.90 -k 1 --spltseed 1
