49eb4dddb485bef72f7461ac14cf236ba431d57f
Michael Gu <michgu@cs.washington.edu>
2024-02-13 20:47:24 -0800

    strd

--- flsim/trainers/sync_trainer.py
+++ flsim/trainers/sync_trainer.py
@@ -305,15 +305,15 @@ class SyncTrainer(FLTrainer):
                                 # thread.start()
 
                                 # crypto
-                                buffer = io.BytesIO()
-                                torch.save(model.fl_get_module().state_dict(), buffer)
-                                buffer.seek(0)
-                                data = buffer.read()
-                                model_hash = hashlib.sha256(data).hexdigest()
-                                mysql_database_helper.insert_model_crypto('localhost', 'michgu', 'test','benchmarks', 'models', model_hash, str(client._name), epoch, round)
+                                # buffer = io.BytesIO()
+                                # torch.save(model.fl_get_module().state_dict(), buffer)
+                                # buffer.seek(0)
+                                # data = buffer.read()
+                                # model_hash = hashlib.sha256(data).hexdigest()
+                                # mysql_database_helper.insert_model_crypto('localhost', 'michgu', 'test','benchmarks', 'models', model_hash, str(client._name), epoch, round)
 
                                 # vanilla
-                                # mysql_database_helper.insert_model('localhost', 'michgu', 'test','benchmarks', 'models', model.fl_get_module().state_dict(), str(client._name), epoch, round)
+                                mysql_database_helper.insert_model('localhost', 'michgu', 'test','benchmarks', 'models', model.fl_get_module().state_dict(), str(client._name), epoch, round)
                     if self.logger.isEnabledFor(logging.DEBUG):
                         norm = FLModelParamUtils.debug_model_norm(
                             self.global_model().fl_get_module()
