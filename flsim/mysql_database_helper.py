24764ed617dfd6d300d73586f7a56f460a57eb26
Michael Gu <michgu@cs.washington.edu>
2024-02-14 11:21:30 -0800

    test

--- flsim/mysql_database_helper.py
+++ flsim/mysql_database_helper.py
@@ -82,7 +82,7 @@ def insert_model_encrypted(db_host, db_user, db_password, db_name, table, encryp
 
     cursor.execute('''
         INSERT INTO `{}`(encrypted_model, encryption_time)
-        VALUES(%s, %s, %s)
+        VALUES(%s, %s)
     '''.format(table), (encrypted_model, encryption_time))
         
     connection.commit()
