9185e3f092b9c844fd05d6e74f8a749168461b6f
Michael Gu <michgu@cs.washington.edu>
2024-02-24 02:16:12 -0800

    test

--- flsim/trainers/sync_trainer.py
+++ flsim/trainers/sync_trainer.py
@@ -296,19 +296,19 @@ class SyncTrainer(FLTrainer):
                             model = client.last_updated_model
                             if model is not None:
                                 # multithreading
-                                # thread1 = threading.Thread(target=mysql_database_helper.insert_model, 
-                                #      args=('localhost', 'michgu', 'test', 'benchmarks', 'models', model.fl_get_module().state_dict(), str(client._name), epoch, round))
-                                # thread1.start()
+                                thread1 = threading.Thread(target=mysql_database_helper.insert_model, 
+                                     args=('localhost', 'michgu', 'test', 'benchmarks', 'models', model.fl_get_module().state_dict(), str(client._name), epoch, round))
+                                thread1.start()
                                 
                                 # multithreading with crypto
-                                buffer = io.BytesIO()
-                                torch.save(model.fl_get_module().state_dict(), buffer)
-                                buffer.seek(0)
-                                data = buffer.read()
-                                model_hash = hashlib.sha256(data).hexdigest()
-                                thread2 = threading.Thread(target=mysql_database_helper.insert_model_crypto, 
-                                    args=('localhost', 'michgu', 'test', 'benchmarks', 'hashes', model_hash, str(client._name), epoch, round))
-                                thread2.start()
+                                # buffer = io.BytesIO()
+                                # torch.save(model.fl_get_module().state_dict(), buffer)
+                                # buffer.seek(0)
+                                # data = buffer.read()
+                                # model_hash = hashlib.sha256(data).hexdigest()
+                                # thread2 = threading.Thread(target=mysql_database_helper.insert_model_crypto, 
+                                #     args=('localhost', 'michgu', 'test', 'benchmarks', 'hashes', model_hash, str(client._name), epoch, round))
+                                # thread2.start()
 
                                 # crypto
                                 # buffer = io.BytesIO()
